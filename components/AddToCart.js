100c4d1a97905d89ffdfbec8510bfba866604aff
Prabhjot <777prabhjot@gmail.com>
2023-10-07 13:02:30 +0530

    update cart section functionality

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,12 +1,31 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/Fontisto";
 import { router } from "expo-router";
+import { useCartStore } from "../store";
 
 const AddToCart = () => {
+  const cartItems = useCartStore((state) => state.cartItems);
+
+  let totalPrice = 0;
+
+  cartItems.forEach((item) => {
+    totalPrice = totalPrice + item?.price;
+  });
   return (
     <>
       <View style={styles.container}>
-        <Text style={{ color: "white" }}>₹ 0.00</Text>
+        <Text style={{ color: "white" }}>₹ {totalPrice}.00</Text>
+        <View style={{ position: "relative" }}>
+          {cartItems?.length ? (
+            <View style={styles.notify}>
+              <Text style={{ color: "white", fontWeight: "600" }}>
+                {cartItems?.length}
+              </Text>
+            </View>
+          ) : (
+            <></>
+          )}
+        </View>
         <TouchableOpacity
           activeOpacity={0.7}
           style={styles.button}
@@ -42,9 +61,22 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    width: "40%",
+    justifyContent: "center",
+    width: "50%",
     paddingHorizontal: 10,
     paddingVertical: 8,
     borderRadius: 25,
+  },
+  notify: {
+    backgroundColor: "#4FAE5A",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    borderRadius: 20,
+    position: "absolute",
+    left: 10,
+    top: -15,
   },
 });
