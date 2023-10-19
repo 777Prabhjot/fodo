68d20442e6ada61b144b84ffd2aeb8c778230c99
Prabhjot <777prabhjot@gmail.com>
2023-10-19 15:46:46 +0530

    added zustand

--- app/index.js
+++ app/index.js
@@ -1,21 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import Banner from "../components/Banner";
 import Categories from "../Screens/Categories";
 import Featured from "../Screens/Featured";
+import { useFonts } from "expo-font";
+import { useLoginStore } from "../store";
+import Login from "../Screens/Login";
 
 const index = () => {
+  const userInfo = useLoginStore((state) => state.user);
+
+  const [fontsLoaded] = useFonts({
+    "Neuton-Italic": require("../assets/fonts/Neuton-Italic.ttf"),
+    "Neuton-Bold": require("../assets/fonts/Neuton-Bold.ttf"),
+    "Neuton-Regular": require("../assets/fonts/Neuton-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={{ backgroundColor: "#FFFFFF" }}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.container}>
-          <Banner />
-          <Categories />
-          <Featured />
+    <>
+      {userInfo ? (
+        <View style={{ backgroundColor: "#FFFFFF", paddingTop: 40 }}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.container}>
+              <Banner />
+              <Categories />
+              <Featured />
+            </View>
+          </ScrollView>
+          <StatusBar style="auto" />
         </View>
-      </ScrollView>
-      <StatusBar style="auto" />
-    </View>
+      ) : (
+        <Login />
+      )}
+    </>
   );
 };
 
